b5e0884754905c589a5f291dc08b12e7e5e48cee
shalkam <shalkamm@gmail.com>
2018-06-29 01:10:23 +0200

    modified operations sagas names

--- src/saga-link.js
+++ src/saga-link.js
@@ -5,11 +5,11 @@ const pubsubLink = new ApolloLink((operation, forward) => {
   const operationName = `${operation.query.definitions[0].operation}.${
     operation.operationName
   }`.toUpperCase();
-  pubsub.publish(`${operationName}`, operation);
+  pubsub.publish(`${operationName}.PENDING`, operation);
   return forward(operation).map(data => {
     const { data: result, errors } = data;
     if (errors) pubsub.publish(`${operationName}.FAIL`, errors);
-    else if (data) pubsub.publish(`${operationName}.SUCESS`, result);
+    else if (data) pubsub.publish(`${operationName}.SUCCESS`, result);
     return data;
   });
 });
