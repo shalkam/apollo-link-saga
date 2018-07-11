86d50b39bcf8d09d6a1436b50057b671f1713de5
shalkam <shalkamm@gmail.com>
2018-07-11 13:07:39 +0200

    removed console.log

--- src/saga-link.js
+++ src/saga-link.js
@@ -2,13 +2,12 @@ import { ApolloLink } from 'apollo-link';
 import pubsub from 'pubsub-js';
 
 const sagaLink = new ApolloLink((operation, forward) => {
-  console.log(operation);
+  console.log(arguments);
   const operationDefinition = operation.query.definitions
                 .find(({kind}) => kind === "OperationDefinition");
   const name = operation.operationName || operationDefinition.selectionSet.selections[0].name.value;
   const operationName = `${operationDefinition.operation}.${name}`.toUpperCase();
   pubsub.publish(`${operationName}.PENDING`, operation);
-  console.log(operationName);
   return forward(operation).map(data => {
     const { data: result, errors } = data;
     if (errors) pubsub.publish(`${operationName}.FAIL`, errors);
